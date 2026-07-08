6d09c9d9a134239eaaea397275adc046fd6bc73d
rambochai7750973-hash <rambochai7750973-hash@users.noreply.github.com>
2026-07-08 16:14:01 +0800

    Fix: add Eclipse Paho and JitPack repositories for Paho dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://repo.eclipse.org/content/repositories/paho-releases/") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
